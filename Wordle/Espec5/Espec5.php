34e8fc609fabec8a4504705d51dc2fddc1640d50
aleeiix10 <aleixbasket2003@gmail.com>
2022-10-20 12:52:31 +0200

    UItims Retocs

--- Wordle/Espec5/Espec5.php
+++ Wordle/Espec5/Espec5.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,91 +10,91 @@
     <title>Pàgina Principal</title>
 
 </head>
-
 <body>
-    <div id="container">
-        <div id="game">
+    <?php
+        $llista = file("catala5.txt");
+        $aleatori = rand(0,count($llista)-1);
+        $paraula= $llista[$aleatori];
+        echo "<script>$paraula</script>\n";
+    ?>
+    <div id="general">
+        <div>
             <header>
-                <h1 class="title">WORDLE</h1>
+                <h1 class="titol">WORDLE</h1>
             </header>
-            <div id="board-container">
-                <div id="board">
-                    <div class="square" id="1"></div>
-                    <div class="square" id="2"></div>
-                    <div class="square" id="3"></div>
-                    <div class="square" id="4"></div>
-                    <div class="square" id="5"></div>
-                    <div class="square" id="6"></div>
-                    <div class="square" id="7"></div>
-                    <div class="square" id="8"></div>
-                    <div class="square" id="9"></div>
-                    <div class="square" id="10"></div>
-                    <div class="square" id="11"></div>
-                    <div class="square" id="12"></div>
-                    <div class="square" id="13"></div>
-                    <div class="square" id="14"></div>
-                    <div class="square" id="15"></div>
-                    <div class="square" id="16"></div>
-                    <div class="square" id="17"></div>
-                    <div class="square" id="18"></div>
-                    <div class="square" id="19"></div>
-                    <div class="square" id="20"></div>
-                    <div class="square" id="21"></div>
-                    <div class="square" id="22"></div>
-                    <div class="square" id="23"></div>
-                    <div class="square" id="24"></div>
-                    <div class="square" id="25"></div>
-                    <div class="square" id="26"></div>
-                    <div class="square" id="27"></div>
-                    <div class="square" id="28"></div>
-                    <div class="square" id="29"></div>
-                    <div class="square" id="30"></div>
+            <div id="paraules-general">
+                <div id="paraules">
+                    <div class="quadrat" id="1"></div>
+                    <div class="quadrat" id="2"></div>
+                    <div class="quadrat" id="3"></div>
+                    <div class="quadrat" id="4"></div>
+                    <div class="quadrat" id="5"></div>
+                    <div class="quadrat" id="6"></div>
+                    <div class="quadrat" id="7"></div>
+                    <div class="quadrat" id="8"></div>
+                    <div class="quadrat" id="9"></div>
+                    <div class="quadrat" id="10"></div>
+                    <div class="quadrat" id="11"></div>
+                    <div class="quadrat" id="12"></div>
+                    <div class="quadrat" id="13"></div>
+                    <div class="quadrat" id="14"></div>
+                    <div class="quadrat" id="15"></div>
+                    <div class="quadrat" id="16"></div>
+                    <div class="quadrat" id="17"></div>
+                    <div class="quadrat" id="18"></div>
+                    <div class="quadrat" id="19"></div>
+                    <div class="quadrat" id="20"></div>
+                    <div class="quadrat" id="21"></div>
+                    <div class="quadrat" id="22"></div>
+                    <div class="quadrat" id="23"></div>
+                    <div class="quadrat" id="24"></div>
+                    <div class="quadrat" id="25"></div>
+                    <div class="quadrat" id="26"></div>
+                    <div class="quadrat" id="27"></div>
+                    <div class="quadrat" id="28"></div>
+                    <div class="quadrat" id="29"></div>
+                    <div class="quadrat" id="30"></div>
                 </div>
             </div>
-            <div id="keyboard-cont">
-                <div class="first-row">
-                    <button data-key="q" class="keyboard-button" onclick="escriureLletra('q')">q</button>
-                    <button data-key="w" class="keyboard-button" onclick="escriureLletra('w')" >w</button>
-                    <button data-key="e" class="keyboard-button" onclick="escriureLletra('e')" >e</button>
-                    <button data-key="r" class="keyboard-button" onclick="escriureLletra('r')" >r</button>
-                    <button data-key="t" class="keyboard-button" onclick="escriureLletra('t')" >t</button>
-                    <button data-key="y" class="keyboard-button" onclick="escriureLletra('y')" >y</button>
-                    <button data-key="u" class="keyboard-button" onclick="escriureLletra('u')" >u</button>
-                    <button data-key="i" class="keyboard-button" onclick="escriureLletra('i')" >i</button>
-                    <button data-key="o" class="keyboard-button" onclick="escriureLletra('o')" >o</button>
-                    <button data-key="p" class="keyboard-button" onclick="escriureLletra('p')" >p</button>
+            <div id="teclat">
+                <div>
+                    <button class="boto-teclat" onclick="escriureLletra('q')">q</button>
+                    <button class="boto-teclat" onclick="escriureLletra('w')" >w</button>
+                    <button class="boto-teclat" onclick="escriureLletra('e')" >e</button>
+                    <button class="boto-teclat" onclick="escriureLletra('r')" >r</button>
+                    <button class="boto-teclat" onclick="escriureLletra('t')" >t</button>
+                    <button class="boto-teclat" onclick="escriureLletra('y')" >y</button>
+                    <button class="boto-teclat" onclick="escriureLletra('u')" >u</button>
+                    <button class="boto-teclat" onclick="escriureLletra('i')" >i</button>
+                    <button class="boto-teclat" onclick="escriureLletra('o')" >o</button>
+                    <button class="boto-teclat" onclick="escriureLletra('p')" >p</button>
                 </div>
-                <div class="second-row">
-                    <button data-key="a" class="keyboard-button" onclick="escriureLletra('a')" >a</button>
-                    <button data-key="s" class="keyboard-button" onclick="escriureLletra('s')" >s</button>
-                    <button data-key="d" class="keyboard-button" onclick="escriureLletra('d')" >d</button>
-                    <button data-key="f" class="keyboard-button" onclick="escriureLletra('f')" >f</button>
-                    <button data-key="g" class="keyboard-button" onclick="escriureLletra('g')" >g</button>
-                    <button data-key="h" class="keyboard-button" onclick="escriureLletra('h')" >h</button>
-                    <button data-key="j" class="keyboard-button" onclick="escriureLletra('j')" >j</button>
-                    <button data-key="k" class="keyboard-button" onclick="escriureLletra('k')" >k</button>
-                    <button data-key="l" class="keyboard-button" onclick="escriureLletra('l')" >l</button>
-                    <button data-key="ñ" class="keyboard-button" onclick="escriureLletra('ñ')" >ñ</button>
+                <div>
+                    <button class="boto-teclat" onclick="escriureLletra('a')" >a</button>
+                    <button class="boto-teclat" onclick="escriureLletra('s')" >s</button>
+                    <button class="boto-teclat" onclick="escriureLletra('d')" >d</button>
+                    <button class="boto-teclat" onclick="escriureLletra('f')" >f</button>
+                    <button class="boto-teclat" onclick="escriureLletra('g')" >g</button>
+                    <button class="boto-teclat" onclick="escriureLletra('h')" >h</button>
+                    <button class="boto-teclat" onclick="escriureLletra('j')" >j</button>
+                    <button class="boto-teclat" onclick="escriureLletra('k')" >k</button>
+                    <button class="boto-teclat" onclick="escriureLletra('l')" >l</button>
+                    <button class="boto-teclat" onclick="escriureLletra('ñ')" >ñ</button>
                 </div>
-                <div class="third-row">
-                    <button data-key="enviar" class="keyboard-button" onclick="enviar()">ENVIAR</button>
-                    <button data-key="z" class="keyboard-button" onclick="escriureLletra('z')" >z</button>
-                    <button data-key="x" class="keyboard-button" onclick="escriureLletra('x')" >x</button>
-                    <button data-key="c" class="keyboard-button" onclick="escriureLletra('c')" >c</button>
-                    <button data-key="v" class="keyboard-button" onclick="escriureLletra('v')" >v</button>
-                    <button data-key="b" class="keyboard-button" onclick="escriureLletra('b')" >b</button>
-                    <button data-key="n" class="keyboard-button" onclick="escriureLletra('n')" >n</button>
-                    <button data-key="m" class="keyboard-button" onclick="escriureLletra('m')" >m</button>
-                    <button data-key="borrar" class="keyboard-button" onclick= "esborrarLletra()">BORRAR</button>
+                <div>
+                    <button class="boto-teclat" onclick="enviar()">ENVIAR</button>
+                    <button class="boto-teclat" onclick="escriureLletra('z')" >z</button>
+                    <button class="boto-teclat" onclick="escriureLletra('x')" >x</button>
+                    <button class="boto-teclat" onclick="escriureLletra('c')" >c</button>
+                    <button class="boto-teclat" onclick="escriureLletra('v')" >v</button>
+                    <button class="boto-teclat" onclick="escriureLletra('b')" >b</button>
+                    <button class="boto-teclat" onclick="escriureLletra('n')" >n</button>
+                    <button class="boto-teclat" onclick="escriureLletra('m')" >m</button>
+                    <button class="boto-teclat" onclick= "esborrarLletra()">BORRAR</button>
                 </div>
             </div>
         </div>
     </div>
-    <?php
-        $array = file("catala5.txt");
-        $rand = rand(0,count($array)-1);
-        $palabra = $array[$rand];
-        echo $palabra;
-?>
+    
 </body>
 </html>
--- Wordle/Espec5/script.js
+++ Wordle/Espec5/script.js
@@ -1,23 +1,22 @@
 let intents = 0;
 let paraulaProbada = [[],[],[],[],[],[]];
 
-
 function escriureLletra(lletra){
     if (paraulaProbada[intents].length<5){
         document.getElementById((intents*5)+(paraulaProbada[intents].length)+1).innerHTML=lletra;
         paraulaProbada[intents].push(lletra);
-        console.log(paraulaProbada[intents]);
     }
+}
 
-}
 function esborrarLletra(){
-    if (paraulaProbada[intents]!=0){
-    document.getElementById(((intents*5)+paraulaProbada[intents].length)).innerHTML= "";
-    paraulaProbada[intents].pop();
-    console.log(paraulaProbada);
+    if(intents<6){
+        if (paraulaProbada[intents]!=0){
+        document.getElementById(((intents*5)+paraulaProbada[intents].length)).innerHTML= "";
+        paraulaProbada[intents].pop();
+        }
     }
-     
 }
+
 function enviar(){
     if (paraulaProbada[intents].length<5){
         alert("La paraula ha de contenir 5 lletres");
@@ -25,6 +24,5 @@ function enviar(){
     else{
         alert("Has enviat la paraula");
         intents +=1;
-    }
-    
+    } 
 }
